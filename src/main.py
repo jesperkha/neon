756aad3062e78a4cca92abc90ae7ab59a88c7080
jesperkha <hammerjesp@gmail.com>
2022-05-19 14:25:39 +0200

    reworded errors

--- src/main.py
+++ src/main.py
@@ -9,13 +9,13 @@ import sys
 def compile():
     args = sys.argv[1:] # omit filename
     if len(args) == 0:
-        util.err("expected input files")
+        util.err("no input files")
 
     filename = args[0]
     if not filename.endswith(".ne"):
-        util.err("expected input file to be a neon file")
+        util.err("input file must be a neon file (ending in '.ne')")
     if not os.path.isfile(filename):
-        util.err(f"could not find file '{filename}'")
+        util.err(f"could not find '{filename}'")
 
     with open(filename, "r") as f:
         ast = parser.parse(lexer.tokenize(f.read()))
