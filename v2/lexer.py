dd0b885e2b106d736dd4c79b6596ea623e4a2fd4
jesperkha <hammerjesp@gmail.com>
2022-07-26 00:58:34 +0200

    cleaned file

--- v2/lexer.py
+++ v2/lexer.py
@@ -8,7 +8,6 @@ class Lexer:
         self.line = 1
         self.col  = -1
         self.idx  = -1
-
         self.string = ""
 
     def tokenize(self) -> list[Token]:
@@ -103,9 +102,12 @@ class Lexer:
                 continue
 
             # Keywords and identifiers
-            if char.isalnum() or char in ("_", "$"):
+            ident_symbols_start = ("_", "$")
+            ident_symbols = ("_")
+
+            if char.isalnum() or char in ident_symbols_start:
                 start_col = self.col
-                word, _ = self.word(lambda c: c.isalnum() or c.isdecimal() or c == "_")
+                word, _ = self.word(lambda c: c.isalnum() or c.isdecimal() or c in ident_symbols)
                 word = char + word
 
                 if word in keyword_lookup:
@@ -142,6 +144,7 @@ class Lexer:
     def add(self, typ: int, lexeme: str, line: int, col: int, isfloat: int = False):
         self.tokens.append(Token(typ, lexeme, line, col, isfloat))
 
+    # Go forward one character, returns character
     def next(self) -> str:
         self.idx += 1
         self.col += 1
@@ -150,6 +153,7 @@ class Lexer:
 
         return ""
 
+    # Go back one character
     def prev(self):
         self.idx -= 1
         self.col -= 1
@@ -171,148 +175,4 @@ class Lexer:
         self.word(lambda _: False)
         util.err(msg, self.string, start_col, end_col, fatal)
         self.skip_line()
-            
-# Returns list of Token from source text
-def tokenize(source: str) -> list[Token]:
-    "returns a list of `Token` with `type`, `lexeme`, and `line` properties. string lexemes include the quotes"
-    token_list = []
-    line = 1
-    idx = -1
-    col = -1
-    string_line = ""
-    
-    while idx < len(source) - 1:
-        idx += 1
-        col += 1
-        char = source[idx]
-        nextchar = "" if idx >= len(source) - 1 else source[idx + 1]
 
-        string_line += char
-
-        # newline
-        if char == "\n":
-            #token_list.append(Token(NEWLINE, "NEWLINE", line))
-            col = -1
-            line += 1
-            string = ""
-            continue
-
-        # ignored whitespace
-        if char in whitespace_lookup:
-            continue
-
-        # strings
-        string_c = ("'", '"')
-        if char in string_c:
-            string  = char
-            start_c = char
-            idx += 1 # skip starting quote
-            terminated = False
-
-            start_col = col
-
-            while idx < len(source):
-                c = source[idx]
-                if c != start_c:
-                    string += c
-                else:
-                    terminated = True
-                    break
-                idx += 1
-                col += 1
-                string_line += c
-            
-            if not terminated:
-                util.err(f"unterminated string, line {line}", string_line, start_col, col)
-
-            # char type
-            string += start_c
-            if start_c == "'":
-                if len(string) != 3: # including ' '
-                    util.err(f"char type must be one character long, line {line}")
-                #token_list.append(Token(CHAR, string, line))
-                continue
-
-            # string type
-            #token_list.append(Token(STRING, string, line))
-            continue
-
-        # symbol
-        if char in symbol_lookup:
-            dbtoken = char + nextchar # double token
-            if dbtoken in double_token_lookup:
-                t = double_token_lookup[dbtoken]
-                
-                # skip to end of line for comments
-                if t == COMMENT:
-                    while idx < len(source):
-                        if source[idx] == "\n":
-                            break
-                        idx += 1
-                    line += 1
-                    continue
-
-                #token_list.append(Token(t, dbtoken, line))
-                idx += 1 # skip next token
-                string_line += nextchar
-                col += 1
-            else:
-                #token_list.append(Token(symbol_lookup[char], char, line))
-                pass
-            continue
-
-        # number
-        if char.isdecimal():
-            num = ""
-            dot = 0
-            while idx < len(source):
-                c = source[idx]
-                if c.isdecimal():
-                    num += c
-                elif c.isidentifier(): # check invalid identifiers to avoid error handling later
-                    util.err(f"cannot start identifier with digit, line {line}")
-                elif c == ".":
-                    num += c
-                    dot += 1
-                else:
-                    break
-                idx += 1
-
-            if dot > 1 or num.endswith("."):
-                util.err(f"invalid number literal '{num}', line {line}")
-            if num.replace(".", "").isdecimal():
-                t = Token(NUMBER, num, line)
-                t.isfloat = dot == 1
-                string_line += t.lexeme[1:]
-                col += len(t.lexeme) -1 
-                #token_list.append(t)
-
-            idx -= 1
-            continue
-
-        # identifier
-        if char.isalnum() or char == "_":
-            word = ""
-            while idx < len(source):
-                c = source[idx]
-                if c.isalnum() or c.isdecimal() or c == "_":
-                    word += c
-                else:
-                    break
-                idx += 1
-        
-            if word in keyword_lookup: # check if reserved keyword first
-                #token_list.append(Token(keyword_lookup[word], word, line))
-                pass
-            else:
-                #token_list.append(Token(IDENTIFIER, word, line))
-                pass
-            idx -= 1
-            string_line += word[1:]
-            col += len(word) - 1
-            continue
-    
-        # fallthrough is error
-        util.err(f"unexpected token '{char}', line {line}")
-
-    return token_list
