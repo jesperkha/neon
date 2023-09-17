73fff9b2a54eac288c17e6d4343b18d0870199d2
jesperkha <hammerjesp@gmail.com>
2023-09-17 22:24:31 +0200

    removed scan step

--- src/main.py
+++ src/main.py
@@ -1,13 +1,11 @@
 import lexer
 import parser
-import scanner
 
 def main(filename: str) -> str:
     with open(filename) as f:
         source = f.read()
         tokens = lexer.get_tokens(source)
         tree   = parser.parse_tokens(tokens)
-        scanner.scan_tree(tree)
         tree.print()
 
 if __name__ == "__main__":
